7e066b49ba0a245555a746668d99a5f13b546c0e
Adam Miner <miner.adam1@hotmail.com>
2018-06-07 09:06:03 -0600

    Early Tweaks

--- javascript/mongo.js
+++ javascript/mongo.js
@@ -4,7 +4,7 @@ const cheerio = require('cheerio');
 const request = require('request');
 const handlebars = require('express-handlebars');
 
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
-
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
