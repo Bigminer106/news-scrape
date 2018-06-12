ef7cc5200bf119b5df8f8c78879b4b4c4578e85e
Adam Miner <miner.adam1@hotmail.com>
2018-06-12 09:01:42 -0600

    Tweaks

--- javascript/mongo.js
+++ javascript/mongo.js
@@ -8,3 +8,21 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines
 
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
+
+console.log('Grabbing Articles from The Onion...');
+
+request('https://www.theonion.com', (err, res, html) => {
+  var $ = cheerio.load(html);
+  var results = [];
+
+  $('a.js_curation-click').each((i, element) => {
+    var title = $(element).text();
+    var link = $(element).children().attr('href');
+
+    results.push({
+      title: title,
+      link: link
+    });
+  });
+  console.log(results);
+});
